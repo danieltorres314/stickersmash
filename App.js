ad4ad3c3e887aba883eb9ea7e0a86da6f0c8b68a
danieltorres314 <daniel.alejandro91314@gmail.com>
2024-07-14 22:17:22 -0500

    button changed 9

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 import ImageViewer from "./components/Imageviewer";
 import Button from "./components/Button";
