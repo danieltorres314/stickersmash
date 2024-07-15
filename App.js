ec9b6759509d1bbe72eca73bf47cfd7f3f5844d0
danieltorres314 <daniel.alejandro91314@gmail.com>
2024-07-14 20:50:38 -0500

    change prettier setting

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <Image source={PlaceholderImage} style={styles.image} />
-    </View>
-      <StatusBar style="auto" />
       </View>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
