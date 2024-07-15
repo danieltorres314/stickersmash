7e3b94f12ed8122cc3a143a8cb5f07f9403d358c
danieltorres314 <daniel.alejandro91314@gmail.com>
2024-07-14 21:05:33 -0500

    imageviewer

---
+++ components/Imageviewer.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Image } from "react-native-web";
+export default function ImageViewer({ placeholderImageSource }) {
+  return <Image source={placeholderImageSource} style={StyleSheet.image} />;
+}
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
