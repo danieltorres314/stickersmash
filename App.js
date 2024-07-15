2c6b4925f08eb9f4b2472680f653235558f3266c
danieltorres314 <daniel.alejandro91314@gmail.com>
2024-07-14 21:09:47 -0500

    imageviewer1

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import ImageViewer from "./components/Imageviewer";
+const PlaceholderImage = require("./assets/images/background-image.png");
+
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -18,5 +21,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#25292e",
     alignItems: "center",
     justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
   },
 });
