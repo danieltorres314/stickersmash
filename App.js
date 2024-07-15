d97e9735fe046e13b614019e7a46a3ac4b7403cb
danieltorres314 <daniel.alejandro91314@gmail.com>
2024-07-14 21:22:07 -0500

    button added

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 import ImageViewer from "./components/Imageviewer";
+import Button from "./components/Button";
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
@@ -9,6 +10,10 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <View style={styles.footerContainer}>
+          <Button label="Choose a photo" />
+          <Button label="Use this photo" />
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -25,5 +30,9 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alighItems: "center",
   },
 });
